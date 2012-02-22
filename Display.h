bce98ed771fe7671973b7678adf88a611b2ccfe8
Fredric Ericsson <fericss@csc.kth.se>
2012-02-22 11:00:55 +0100

    Tidig pseudo kod

---
+++ Display.h
@@ -0,0 +1,18 @@
+/*
+ * Display.h
+ *
+ *  Created on: Feb 22, 2012
+ *      Author: fericss
+ */
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+class Display
+{
+	public:
+
+	private:
+
+
+}
+#endif /* DISPLAY_H_ */
