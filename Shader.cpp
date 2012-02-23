42885998402dd5fa4a098775808de62c926f613b
David Nilsson <nilsson.dd+github+gravatarobscurity@gmail.com>
2012-02-23 23:14:56 +0100

    Implementerar samtliga metoder samt en ny metod för att hämta programid. Hittills inte testad/kompilerad.
    Den färdiga klassen kanske underlättar implementationen av colortranslator #002

--- Shader.cpp
+++ Shader.cpp
@@ -1,4 +1,4 @@
-//============================================================================
+/============================================================================
 // Name        : Shader.cpp
 // Author      : David och Fredric E.
 // Description : Shader class implementation.
@@ -17,28 +17,87 @@
 using namespace std;
 
 /*
-int main() {
-	// Endast för test
-	//todo Tas bos när vi har testat klart
-	Shader *bla = new Shader("what","eek");
-	(*bla).addParameter("harr", 2);
+   int main() {
+// Endast för test
+//todo Tas bos när vi har testat klart
+Shader *bla = new Shader("what","eek");
+(*bla).addParameter("harr", 2);
 
-	cout << bla->getParameters()[0].param;
-	(*bla).addParameter("yarr", 3);
-	(*bla).removeParameter("harr");
-	cout << bla->getParameters()[0].param;
-	return 0;
+cout << bla->getParameters()[0].param;
+(*bla).addParameter("yarr", 3);
+(*bla).removeParameter("harr");
+cout << bla->getParameters()[0].param;
+return 0;
 }
-*/
+ */
 
 /**
  * Shader constructor.
  * @param vsPath path to the vertex shader file.
+ * An empty string indicates no shader.
  * @param fsPath path to the fragment shader file.
+ * An empty string indicates no shader.
  */
-Shader::Shader(std::string vsPath, std::string fsPath)
+Shader::Shader(std::string vsPath, std::string fsPath) : vertexId(0), fragId(0), progId(0)
 {
+    if(vsPath != "") {
+        vertexId = compileShader(vsPath, GL_VERTEX_SHADER);
+    }
 
+    if(fsPath != "") {
+        fragId = compileShader(fsPath, GL_FRAGMENT_SHADER);
+    }
+
+    if(vertexId && fragId) {
+        progId = compileProgram();
+    }
+}
+
+/**
+ * Compile the given shader and return ID.
+ * @param path Path to the shader source file.
+ * @param type Type of shader.
+ * @return Shader ID.
+ */
+GLuint Shader::compileShader(std::string path, GLenum type)
+{
+    GLuint shader = glCreateShader(type);
+    const char * filePointer = strShaderFile.c_str();
+    glShaderSource(shader, 1, &filePointer, NULL);
+
+    glCompileShader(shader);
+
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        throw "Error in shader compilation";
+    }
+
+    return shader;
+}
+
+/**
+ * Compile shaders into a program.
+ * @return Program ID.
+ */
+GLuint Shader::compileProgram()
+{
+    GLuint program = glCreateProgram();
+
+    glAttachShader(program, fragId);
+    glAttachShader(program, vertexId);
+
+    glLinkProgram(program);
+
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        throw "Error in program compilation";
+    }
+
+    return program;
 }
 
 /**
@@ -47,6 +106,11 @@ Shader::Shader(std::string vsPath, std::string fsPath)
  */
 void Shader::setVertexShader(GLuint openglRef)
 {
+    vertexId = openglRef;
+    if(vertexId && fragId) {
+        //TODO: delete program and recompile
+        progId = compileProgram();
+    }
 }
 
 /**
@@ -55,6 +119,11 @@ void Shader::setVertexShader(GLuint openglRef)
  */
 void Shader::setFragmentShader(GLuint openglRef)
 {
+    fragId = openglRef;
+    if(vertexId && fragId) {
+        //TODO: delete program and recompile
+        progId = compileProgram();
+    }
 }
 
 /**
@@ -63,6 +132,7 @@ void Shader::setFragmentShader(GLuint openglRef)
  */
 GLuint Shader::getVertexShader()
 {
+    return(vertexId);
 }
 
 /**
@@ -71,15 +141,24 @@ GLuint Shader::getVertexShader()
  */
 GLuint Shader::getFragmentShader()
 {
+    return(fragId);
 }
 
+/**
+ * Get the current shader program.
+ * @return openglRef ID of the shader program.
+ */
+GLuint Shader::getShaderProgram()
+{
+    return(progId);
+}
 
 /**
  * Get the vector with all variables associated with this shader.
  * @return A vector with all variables
  */
 std::vector<uniform_t> Shader::getParameters(){
-	return uniforms;
+    return uniforms;
 }
 
 /**
@@ -89,11 +168,11 @@ std::vector<uniform_t> Shader::getParameters(){
  */
 
 void Shader::removeParameter(std::string name){
-	for(unsigned int i = 0; i<uniforms.size();i++){
-		if(name.compare(uniforms[i].param)==0){
-			uniforms.erase(uniforms.begin()+i);
-		}
-	}
+    for(unsigned int i = 0; i<uniforms.size();i++){
+        if(name.compare(uniforms[i].param)==0){
+            uniforms.erase(uniforms.begin()+i);
+        }
+    }
 }
 
 /**
@@ -103,6 +182,6 @@ void Shader::removeParameter(std::string name){
  * @param p GLuint p is a OpenGL reference pointing to a variable with name name.
  */
 void Shader::addParameter(std::string name, GLuint p){
-	uniforms.push_back(Uniforms_struct(name, p));
+    uniforms.push_back(Uniforms_struct(name, p));
 }
 
--- Shader.h
+++ Shader.h
@@ -14,31 +14,32 @@
 #include <glload/gl_3_2_comp.h>
 #include <GL/freeglut.h>
 
-
-
 typedef struct Uniforms_struct{
-	std::string param;
-	GLuint id;
-	Uniforms_struct(std::string a, GLuint b){
-		param = a;
-		id = b;
-	}
-
+  std::string param;
+  GLuint id;
+  Uniforms_struct(std::string a, GLuint b){
+    param = a;
+    id = b;
+  }
 } uniform_t;
 
 class Shader {
-public:
-	Shader(std::string vsPath, std::string  fsPath);
-	void setVertexShader(GLuint openglRef);
-	void setFragmentShader(GLuint openglRef);
-	GLuint getVertexShader();
-	GLuint getFragmentShader();
-	std::vector<uniform_t> getParameters();
-	void removeParameter(std::string name);
-	void addParameter(std::string name, GLuint p);
-private:
-	GLuint id;
-	std::vector<uniform_t> uniforms;
+  public:
+    Shader(std::string vsPath, std::string  fsPath);
+    void setVertexShader(GLuint openglRef);
+    void setFragmentShader(GLuint openglRef);
+    GLuint getVertexShader();
+    GLuint getFragmentShader();
+    GLuint getShaderProgram();
+    std::vector<uniform_t> getParameters();
+    void removeParameter(std::string name);
+    void addParameter(std::string name, GLuint p);
+  private:
+    GLuint compileShader(std::string path, GLenum type);
+    GLuint compileProgram();
+
+    GLuint progId, fragId, vertexId;
+    std::vector<uniform_t> uniforms;
 };
 
 #endif /* SHADER_H_ */
