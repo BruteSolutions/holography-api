5f6fb00acab2b3bde0b24dbeb0513336ae852f41
Fredric Ericsson <fericss@kth.se>
2012-02-23 12:37:21 +0100

    Initial commit.

---
+++ Shader.cpp
@@ -0,0 +1,52 @@
+//============================================================================
+// Name        : Shader.cpp
+// Author      : David och Fredrik E.
+// Description : Shader class implementation.
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Shader.h"
+
+using namespace std;
+
+/**
+ * short description.
+ * @param
+ * @return
+ */
+Shader(String vsPath, String fsPath)
+{
+
+}
+
+void Shader::setVertexShader(GLuint openglRef)
+{
+}
+
+void Shader::setFragmentShader(GLuint openglRef)
+{
+}
+
+GLuint Shader::getVertexShader()
+{
+}
+
+Gluint Shader::getFragmentShader()
+{
+}
+
+String Shader::getParameters()
+{
+}
+
+void Shader::removeParameter(String name)
+{
+}
+
+void Shader::addParameter(String name, GLuint p)
+{
+}
+
