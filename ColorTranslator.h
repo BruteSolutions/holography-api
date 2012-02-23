fe148c27236a4f7518a3813dce30db084b0021b0
Fredrik Frantzen <ffra@csc.kth.se>
2012-02-23 10:45:56 +0100

    Skapade ColorTranslator

---
+++ ColorTranslator.h
@@ -0,0 +1,17 @@
+#ifndef __ColorTranslator_H
+#define __ColorTranslator_H
+
+#include "structs.h"
+class Shader{};
+
+class ColorTranslator{
+private: 
+	ColorVec3 factor;
+	Shader shader;
+public:
+	ColorTranslator(ColorVec3 &factor);
+	void setConversionFactor(ColorVec3 &factor);
+	ColorVec3 &getConversationFactor(); 
+	void apply(ColorVec4 *color[], int numTriangles);
+};
+#endif
---
+++ testColorTranslator.cpp
@@ -0,0 +1,7 @@
+#include "structs.h"
+#include "ColorTranslator.h"
+
+int main ( ){
+
+	return 0;
+}
