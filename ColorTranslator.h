c8cfc9b5d14985889a37fae370d741ba82192215
Fredrik Frantzen <ffra@kth.se>
2012-03-01 17:00:32 +0100

    Så här tror jag att den ska se ut, och man ska istället lägga in en uniform vector i opengls buffer och sedan bara anropar shadern.

--- ColorTranslator.h
+++ ColorTranslator.h
@@ -1,32 +1,50 @@
 #ifndef __ColorTranslator_H
 #define __ColorTranslator_H
+#include <string>
 #include "structs.h"
+
 //Class might be redundant as everything can be done with a shader
 class Shader{};
 
 class ColorTranslator{
 private: 
-	ColorVec3 factor;
-	Shader shader;
+    float *factor;
+    std::string redmonochromeShader (
+        "#version 330\n"
+	      "in vec4 inColor;\n"
+	      "uniform vec3 factor;\n"
+	      "out vec4 outColor;\n"
+	      "void main()\n"
+	      "{\n"
+	      " float r = inColor.x*factor.x;\n"
+	      " float g = inColor.y*factor.y;\n"
+	      " float b = inColor.z*factor.z;\n"
+	      " outColor = vec4( r + g + b, 0.0f, 0.0f, 1.0f);\n"
+	      "}\n"
+    );
+	  Shader shader;
 public:
-	ColorTranslator(ColorVec3 factor){
-		setConversionFactor(factor);
-//		this->shader = NULL; //Shader("","redmonochrome.frag");
-	}
-
-	void setConversionFactor(ColorVec3 factor){
-		this->factor = factor;
-	}
-	ColorVec3 &getConversionFactor(){
-		return factor;
-	} 
-	void apply(float * colors, int size){ //Get whole color buffer for colors, apply factor
-		for(int i = 0; i < size; i=i+4){
-				colors[i]=colors[i]*factor.r;		//r
-				colors[i+1]=colors[i+1]*factor.g;	//g
-				colors[i+2]=colors[i+2]*factor.b;	//b
+	  ColorTranslator(float *factor){
+		    setConversionFactor(factor);
+	  }
+    ~ColorTranslator(){
+        delete factor;
+    }
+	  void setConversionFactor(float *factor){
+		    this->factor = factor;
+	  }
+	  float* getConversionFactor(){
+		    return factor;
+	  }
+/* redundant method?
+   void apply(float * colors, int size){ 
+	  //Get whole color buffer for colors, apply factor
+		    for(int i = 0; i < size; i=i+4){
+				    colors[i]=colors[i]*factor.r;		//r
+				    colors[i+1]=colors[i+1]*factor.g;	//g
+				    colors[i+2]=colors[i+2]*factor.b;	//b
 													//w
-		}		
-	}
+		    }
+	  }*/
 };
 #endif
