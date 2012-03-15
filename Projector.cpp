3952ede9983e3b759e8e9eff0f0639c787198c02
Fredrik <fredde90__@hotmail.com>
2012-03-15 10:45:24 +0100

    La till projector

---
+++ Projector.cpp
@@ -0,0 +1,183 @@
+#include <list>
+#include "Projector.h"
+#include "Vec3.cpp"
+
+/*
+ * Creates a new projector with buffer of size len at the place pointer with position pos
+ * and direction dir;
+ */
+Projector::Projector(int * pointer, unsigned int len, Vec3 pos, Vec3 dir) : highlighted(false) 
+{
+	colorTranslator = new ColorTranslator(); //Creates a ColorTranslator with default values.
+	pnt = pointer;
+	bufferSize = len;
+	setPosition(pos);
+	setDirection(dir);
+	//create necessary Shader objects => hardcode a picture for the buffer. How do I do that?
+}
+
+/*
+ * Creates a new projector with buffer of size len at the place pointer with position (0,0,0)
+ * and direction (0,0,0);
+ */
+Projector::Projector(int * pointer, unsigned int len)
+{	
+	//Projector(pointer, len, new Vec3(0, 0, 0), new Vec3 (0, 0, 0));
+	Vec3 pos = {0,0,0}, dir = {0,0,0};
+	Projector(pointer, len, pos, dir);
+
+}
+/*
+ * Sets the position of the projector to the value of the parametre.
+ * @param The new position of the projector.
+ */
+void Projector::setPosition(Vec3 pos)
+{
+	this.pos = pos;
+}
+
+/*
+ * Returns the position of the projector as a Vec3 object.
+ * @return the position of the projector
+ */
+Vec3 Projector::getPosition()
+{
+	return pos;
+}
+
+/*
+ * Sets the direction of the projectior to the parametre.
+ * @param The new direction of the projector
+ */
+void Projector::setDirection(Vec3 dir)
+{
+	this.dir = dir;
+}
+
+/*
+ * Returns the direction of the projector as a Vec3 object.
+ * @return the direction of the projector
+ */
+Vec3 Projector::getDirection()
+{
+	return dir;
+}
+
+/*
+ * Returns the size of the buffer.
+ * @return Size of the buffer.
+ */
+unsigned int Projector::getBufferSize()
+{
+  return(bufferSize);
+}
+
+/*
+  Returns the associated colortranslator object.
+ * @return Colortranslator object.
+ */
+ColorTranslator Projector::getColorTranslator()
+{
+  return(colorTranslator);
+}
+
+/*
+ * Set the current configuration.
+ * @param c Configuration to be used.
+ */
+void Projector::setConfiguration(Configuration c)
+{
+  configuration = c;
+}
+
+/*
+ * Returns the current configuration.
+ * @return Current configuration.
+ */
+Configuration Projector::getConfiguration()
+{
+  return(configuration);
+}
+
+/*
+ * Highlights the background.
+ */
+void Projector::highlight()
+{
+  if(!highlight) {
+    highlight = true;
+    //TODO: Add
+  }
+}
+
+/*
+ * Disables highlighting of background.
+ */
+void Projector::unHighlight()
+{
+  if(highlight) {
+    highlight = false;
+    //TODO: Remove
+  }
+}
+
+/*
+ * Gets a pointer to the memory location of the buffer used by this certain projector.
+ * @param
+ * @return
+ */
+int* Projector::getBuffer()
+{
+	return &buffer;
+}
+
+/*
+ * Displays the view through the projector associated with this instance.
+ * Prior to that it will also possibly have applied color settings and
+ * definitely applied keystone correction on the view.
+ * @param
+ * @return
+ */
+void Projector::display (Scene scn)
+{
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  //TODO: Do color settings
+
+  3Dspace space = scn.get3DSpace();
+  std::list<GraphicalObject> goList = space.getObjects(); // I need a list to do for each.
+
+  for (std::list<GraphicalObject>::iterator it = goList.begin(); goList != fifth.end(); goList++) {
+    if(map.containsKey(*it.id)) {
+      //TODO: Apply associated shaders
+    }
+  }
+
+  //TODO: Perform keystone correction
+  
+  glutSwapBuffers();
+}
+
+/*
+ * Gets the list of in use shaders.
+ * @param void;
+ * @return std::list<Shader>
+ */
+
+std::list<Shader> Projector::getShaders()
+{
+  return shaders;
+}
+
+/*
+ * Adds a shader "s" to the list of shaders.
+ // * This might endlessly grow, to later crash the program?
+ // * Due to the fact that there is no way to effectivly remove shaders.
+ * @param Shader s, the Shader you want to add to the list.
+ * @return void
+ */
+void Projector::addShader(Shader s)
+{
+  shaders.add(s);  
+}
---
+++ Projector.h
@@ -0,0 +1,35 @@
+#ifndef PROJECTOR_H_
+#define PROJECTOR_H_
+
+#include "ColorTranslator.h"
+
+class Projector
+{
+public:
+  Projector(int * pointer, unsigned int len);
+  Projector(int * pointer, unsigned int len, Vec3 pos, Vec3 dir);
+  unsigned int getBufferSize();
+  ColorTranslator getColorTranslator();
+  void setConfiguration(Configuration c);
+  Configuration getConfiguration();
+  void highlight();
+  void unHighlight();
+  int* getBuffer();
+  void display(Scene scn);
+  void addShader(Shader s);
+  Vec3 getPosition();
+	Vec3 getDirection();
+  void setPosition(Vec3 pos);
+  void setDirection(Vec3 pos);
+private:
+  std::list<Shader> shaders;
+  int * pnt;
+  unsigned int bufferSize;
+  ColorTranslator colorTranslator;
+  Configuration configuration;
+  bool highlighted;
+  Vec3 pos, dir;
+  
+};
+
+#endif
