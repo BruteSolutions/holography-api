4207dabefad1c891340afaf7ed1a39a6e2efffb8
tompet0191 <skylines123@gmail.com>
2012-05-06 14:27:01 +0300

    Kommenterad

--- UniversalConfiguration.cpp
+++ UniversalConfiguration.cpp
@@ -15,17 +15,23 @@
 using namespace std;
 
 
-
+/* Creates a UniversalConfiguration object with the given parameters. */
 UniversalConfiguration::UniversalConfiguration(vector<Configuration> cs, ColorTranslator ct){
 	configurations = cs;
 	colorTrans = ct;
 }
+
+/* Returns a list of Configuration objects each describing the configurations of a projector. */
 vector<Configuration> UniversalConfiguration::getProjectorConfigurations(){
 	return configurations;
 }
+
+/*  Returns the ColorTranslator object that should be used by the Display class. */
 ColorTranslator* UniversalConfiguration::getColorTranslator(){
 	return &colorTrans;
 }
+
+/* Reads the underlying stream and constructs a UniversalConfiguration object which is returned */
 UniversalConfiguration* UniversalConfiguration::readStream(ifstream& is){
    Vec3 Tfactor, Tpos, Tdir;
    is >> Tfactor.x >> Tfactor.y  >> Tfactor.z;
@@ -35,6 +41,7 @@ UniversalConfiguration* UniversalConfiguration::readStream(ifstream& is){
    return new UniversalConfiguration(Tpos, Tdir, Tfactor);
    
 }
+/* Writes this object to the underlying stream. */
 void UniversalConfiguration::writeToStream(ofstream& os){
    os << factor.x << factor.y  << factor.z;
    os << pos.x << pos.y  << pos.z;
