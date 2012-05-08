efd11d9c04535e4c715d67d4998b059ca072f729
tompet0191 <skylines123@gmail.com>
2012-05-08 19:01:28 +0300

    Update Configuration.cpp

--- Configuration.cpp
+++ Configuration.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : Configuration.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <fstream>
 #include "Shared.h"
@@ -41,40 +33,38 @@ using namespace std;
 
 /* Constructs a Configuration object with the given parameters as instance variables. */
 Configuration::Configuration(Vec3 _pos, Vec3 _dir, Vec3 _factor){
-   pos = _pos;
-   dir = _dir;
-   factor = _factor;
+    pos = _pos;
+    dir = _dir;
+    factor = _factor;
 }
 
 /* Returns the ColorTranslator that should be used by the projector described. */
 Vec3 Configuration::getPosition(){
-   return pos;
+    return pos;
 }
 
 /* Returns the position of the projector. */
 Vec3 Configuration::getDirection(){
-   return dir;
+    return dir;
 }
 
 /* Returns the direction of the projector. */
 Vec3 Configuration::getColorTranslatorFactor(){
-   return factor;
+    return factor;
 }
 
 /* Writes this object to the underlying stream. */
 void Configuration::writeToStream(std::ofstream& os){
-	os << factor.x << factor.y  << factor.z;
-	os << pos.x << pos.y  << pos.z;
-	os << dir.x << dir.y  << dir.z;
+    os << factor.x << factor.y  << factor.z;
+    os << pos.x << pos.y  << pos.z;
+    os << dir.x << dir.y  << dir.z;
 }
 
 /* Reads the underlying stream and constructs a Configuration object which is returned. */
 Configuration* Configuration::readStream(std::ifstream& is){
-	Vec3 Tfactor;
-	Vec3 Tpos;
-	Vec3 Tdir;
-	is >> Tfactor.x >> Tfactor.y  >> Tfactor.z;
-	is >> Tpos.x >> Tpos.y  >> Tpos.z;
-	is >> Tdir.x >> Tdir.y  >> Tdir.z;
-   return new Configuration(Tpos, Tdir, Tfactor);
+    Vec3 Tfactor, Tpos, Tdir;
+    is >> Tfactor.x >> Tfactor.y  >> Tfactor.z;
+    is >> Tpos.x >> Tpos.y  >> Tpos.z;
+    is >> Tdir.x >> Tdir.y  >> Tdir.z;
+    return new Configuration(Tpos, Tdir, Tfactor);
 }
