0ad3434096c7de9c196c2f016cb87cd74d4a9af6
ffra <ffra@kth.se>
2012-05-10 09:25:45 +0300

    la till implementation som saknades i skelettet, har inte påbörjat GTK ännu

--- src/View.cpp
+++ src/View.cpp
@@ -3,13 +3,13 @@
  * Metoder startade. Vissa saknar implementering då jag inte hittar
  * lämpliga metoder för det i Display.
  *
- * Är ej heller grafisk på något sätt. Behöver mergas med påbörjad grafisk variant.
+ * Är ej heller grafisk på något sätt. Behöver mergas med påbörjad grafisk variant. (GTK)
  *
  * Bör inte View ha en referens till Controller då det grafiska måste
  * kunna uppdatera Controller?
  */
 View::View(){
-	View::View(null);
+	this = new View(NULL);
 }
 
 View::View(Display display){
@@ -22,10 +22,16 @@ View::View(Display display){
  * Sets all projectors red factor. This
  * method should update Scene when it is changed.
  */
-void View::setProjectorsRedFactor(int rFactor){
-	View::display.
-	//TODO
-	//Don't know how to do this
+void View::setProjectorsRedFactor(int rFactor, int gFactor, int bFactor){
+	// decide whether to throw error or set to limits
+	if(rFactor > 255) rFactor = 255;
+	if(gFactor > 255) gFactor = 255;
+	if(bFactor > 255) bFactor = 255;
+	if(rFactor < 0) rFactor = 0;
+	if(gFactor < 0) gFactor = 0;
+	if(bFactor < 0) bFactor = 0;
+	Vec3 factor = { (float) rFactor/255, (float) gFactor/255, (float) bFactor/255)}; 
+	display->getColorTranslator().setConversionFactor(factor);
 }
 
 /*
@@ -34,11 +40,11 @@ void View::setProjectorsRedFactor(int rFactor){
  */
 void View::highlightBackground(bool h){
 	if(h && (!highlightBackground)){
-		display.highlight();
+		display->highlight();
 		highlightBackground=true;
 	}
 	if(!h && highlightBackground){
-		display.unHighlight();
+		display->unHighlight();
 		highlightBackground=false;
 	}
 }
@@ -48,40 +54,35 @@ void View::highlightBackground(bool h){
  * one projector’s colour balancing needs to be adjusted.
  */
 Projector * View::getProjector(int index){
-	//TODO
-	//Don't know how to do this. Can't find any methods for it
-	return null;
+	return display->getProjectors().at(index);
 }
 
 /*
  * Returns the Display used
  */
 Display * View::getDisplay(){
-	return Display;
+	return display;
 }
 
 /*
  * A grid is projected by the projectors if g is true instead of a
  * possible 3D world. If g is false a possible 3D world is shown.
  */
-void View::toggleGrid(bool g){
+void View::toggleGrid(bool g = !showgrid){ /*default value parameter */
 	showgrid=g;
-	//Don't know how to do this. Can't find any methods for it
-	//TODO
+	//TODO, method in Display that sets a grid
 }
 
 /*
  * Sets the display to the display used as input.
  */
-void View::setDisplay(Display display){
-	this.display=display;
+void View::setDisplay(Display * display){
+	this->display=display;
 }
 
 /*
  * Updates the View by render
  */
 void View::updateView(Scene s){
-	//TODO
-	//Don't know how to do this. Can't find any methods for it
+	display->display(s);
 }
- 
