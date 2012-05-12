24d7d4504c21565f39023eca346969b1458a579c
ffra <ffra@kth.se>
2012-05-12 11:58:16 +0300

    la till exceptions

--- include/X3DLoader.h
+++ include/X3DLoader.h
@@ -24,16 +24,11 @@ struct Vec3Int {
 
 class X3DLoader : FileLoader {
 public:
-    static Scene* loadFile(std::string path);
+    static Scene* loadFile( std::string path ) throws ( std::string );
 
 private:
-    /*
-    * input is all the coordIndexes that should be copied into faceVector
-    * if we are reading multiple shapes the coordindex should tell this method
-    * where the coordindex for the next shape should start.
-    */
-    static void copyCoordIndex(const char* input, std::vector<Vec3Int> *faceVector, int coordOffset);
-    static void copyPoints(const char* input, std::vector<Vec4> *pointVector);
+    static void copyCoordIndex( const char* input, std::vector<Vec3Int> *faceVector, int coordOffset );
+    static void copyPoints( const char* input, std::vector<Vec4> *pointVector );
 };
 
 #endif
