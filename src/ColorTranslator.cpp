2e27f414e3add09bd8bb6129b46d2ce4ab26d039
Jimipe <hesj90@gmail.com>
2012-05-16 18:51:41 +0300

    Exceptions fixade enligt dokument. Har bortsett från summa av faktorer är större än 1 då detta hanteras genom att faktorerna normaliseras istället.

--- src/ColorTranslator.cpp
+++ src/ColorTranslator.cpp
@@ -7,58 +7,63 @@
 #include "ColorTranslator.h"
 #include "Shared.h"
 
-
 /*
  * Constructs a new ColorTranslator with a standard conversion factor.
  */
-ColorTranslator::ColorTranslator() throw ( std::string ) {
+ColorTranslator::ColorTranslator() throw (std::string) {
     Vec3 factor = { 0.217f, 0.715f, 0.017f };
-    setConversionFactor( factor );
-    shader = new Shader( "main.vert","main.frag" );
+	setConversionFactor(factor);
+	shader = new Shader("main.vert", "main.frag");
 }
 
 /*
  * Constructs a new ColorTranslator with the given conversion factor.
- * @param factor a vector of 3 floats that determine the amount of red, green 
- * that will be converted to red. 
+ * @param factor a vector of 3 floats that determine the amount of red, green
+ * that will be converted to red.
  */
-ColorTranslator::ColorTranslator( Vec3 factor ) throw ( std::string ) {
-    setConversionFactor( factor );
-    shader = new Shader( "main.vert","main.frag" );
+ColorTranslator::ColorTranslator(Vec3 factor) throw (std::string) {
+	setConversionFactor(factor);
+	shader = new Shader("main.vert", "main.frag");
 }
 
 /*
  * Destructor.
  */
 ColorTranslator::~ColorTranslator() {
-    delete shader;
+	delete shader;
 }
 
 /*
  * Sets the factor of this class.
  * If the sum of the components in factor is bigger than 1 normalize the components.
- * @param factor a vector of 3 floats that determine the amount of red, green 
+ * @param factor a vector of 3 floats that determine the amount of red, green
  * that will be converted to red.
  */
-void ColorTranslator::setConversionFactor( Vec3 factor ) throw ( std::string ) {
-    if( factor.x < 0 || factor.y < 0 || factor.z < 0 )
-        throw std::string("IllegalArgumentException(A conversion-factor is less than 0.)");
-    float factorSum = factor.x + factor.y + factor.z;
-    if( factorSum > 1 ){ //normalize components so that the sum will be one
-    	factor.x = factor.x / factorSum;
-        factor.y = factor.y / factorSum;
-        factor.z = factor.z / factorSum;
-    }
-    this->factor = factor;
+void ColorTranslator::setConversionFactor(Vec3 factor) throw (std::string) {
+	if (factor.x < 0)
+		throw std::string(
+				"IllegalArgumentException(The first conversion-factor is less than 0.)");
+	if (factor.y < 0)
+		throw std::string(
+				"IllegalArgumentException(The second conversion-factor is less than 0.)");
+	if (factor.z < 0)
+		throw std::string(
+				"IllegalArgumentException(The third conversion-factor is less than 0.)");
+	float factorSum = factor.x + factor.y + factor.z;
+	if (factorSum > 1) { //normalize components so that the sum will be one
+		factor.x = factor.x / factorSum;
+		factor.y = factor.y / factorSum;
+		factor.z = factor.z / factorSum;
+	}
+	this->factor = factor;
 }
 
 /*
  * Returns a pointer to the factor of this class
  * @return the factor of this class.
  */
-Vec3 ColorTranslator::getConversionFactor() const 
-{
-    return factor;
+Vec3 ColorTranslator::getConversionFactor() const {
+	return factor;
 }
 
 /*
@@ -66,13 +71,14 @@ Vec3 ColorTranslator::getConversionFactor() const
  * @return a pointer to the shader object of this class.
  */
 Shader* ColorTranslator::getShader() const {
-    return shader; 
+	return shader;
 }
 
 /*
  * Sets the uniform "factor" so that the fragment shader can access the factor of this class.
  */
 void ColorTranslator::setFactorUniform() {
-    GLuint factorLocation = glGetUniformLocation( shader->getShaderProgram(), "factor" );
-    glUniform3f( factorLocation, factor.x, factor.y, factor.z );
+	GLuint factorLocation = glGetUniformLocation(shader->getShaderProgram(),
+			"factor");
+	glUniform3f(factorLocation, factor.x, factor.y, factor.z);
 }
