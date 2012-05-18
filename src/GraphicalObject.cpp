51f70b6162495e51db2002f85710c2d6003691f2
Fredrik Hillnertz <fredde90__@hotmail.com>
2012-05-18 12:35:11 +0200

    Kommentarer p GraphicalObject.cpp

--- src/GraphicalObject.cpp
+++ src/GraphicalObject.cpp
@@ -9,6 +9,14 @@
 GraphicalObject::GraphicalObject() {
 }
 
+/**
+ * GraphicalObject constructor
+ * @param _vertexData Array containing the vertex data.
+ * @param _vertexDataSize Should be the number of floats in _vertexData.
+ * @param _colorData Array containing the color data for the object.
+ * @param _colorDataSize Should be the number of floats in _colorData.
+ * In most cases is _colorDataSize = _vertexDataSize.
+ */
 GraphicalObject::GraphicalObject(float _vertexData[], int _vertexDataSize, float _colorData[], int _colorDataSize) {	
 	vertexData = _vertexData;
 	colorData = _colorData;	
@@ -40,23 +48,41 @@ GraphicalObject::GraphicalObject(float _vertexData[], int _vertexDataSize, float
 	angleZ = 0;
 }
 
-
+/**
+ * Returns the objects vertex data array.
+ * @return Objects vertex data array.
+ */
 float* GraphicalObject::getVertexData() {	
 	return vertexData;
 }
 
+/**
+ * Returns the objects color data array.
+ * @return Objects color data array.
+ */
 float* GraphicalObject::getColorData() {	
 	return colorData;
 }
 
+/**
+ * Returns the color data vertex size.
+ * @return Color data vertex size.
+ */
 int GraphicalObject::getVertexDataSize() {	
 	return vertexDataSize;
 }
 
+/**
+ * Returns the color data array size.
+ * @return Color data array size.
+ */
 int GraphicalObject::getColorDataSize() {	
 	return colorDataSize;
 }
 
+/**
+ * Draws the object.
+ */
 void GraphicalObject::draw() {
 	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
 
@@ -76,6 +102,10 @@ void GraphicalObject::draw() {
 	glDisableVertexAttribArray(1);
 }
 
+/**
+ * Binds the buffer data of colarData and vertexData. 
+ * Must be done before calling the method draw().
+ */
 void GraphicalObject::bindBufferData() {
 	glGenBuffers(1, &positionBufferObject);
 	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
@@ -88,6 +118,11 @@ void GraphicalObject::bindBufferData() {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glFlush();
 }
+
+/**
+ * Calculates the rotation matrixes needed by the shader to rotate the object
+ * based on the rotation variables angleX, angleY ,angleZ.
+ */
 void GraphicalObject::rotate() {
 	objectRotX.m[5] = cos(angleX);
 	objectRotX.m[6] = -sin(angleX);
@@ -105,10 +140,22 @@ void GraphicalObject::rotate() {
 	objectRotZ.m[5] = cos(angleZ); 
 
 }
+
+/**
+ * Sets the highlight uniform so it can be accessed by the shader.
+ * @param shader The shader that uses the highlight uniform.
+ */
 void GraphicalObject::setHighlightUniform(GLuint shader){
 	GLuint highlightLoc = glGetUniformLocation(shader, "highlight");
     glUniform1f(highlightLoc, highlight);
 }
+
+/**
+ * Sets the scale, objectRotX, objectRotY, objectRotZ and objOrigin 
+ * uniforms so they can be accessed by the shader.
+ * @param shader The shader that uses the scale, objectRotX, 
+ * objectRotY, objectRotZ and objOrigin uniforms.
+ */
 void GraphicalObject::applyRotation(GLuint shader) {
 	/*	
 	for(int i = 0; i < 16; i++){
@@ -139,57 +186,108 @@ void GraphicalObject::applyRotation(GLuint shader) {
     GLuint originLoc = glGetUniformLocation(shader, "objOrigin");
     glUniform3f(originLoc, origin.x,origin.y,origin.z);
 }
+
+/**
+ * Sets the highlight boolean.
+ * @param _bool The new highlight value.
+ */
 void GraphicalObject::setHighlight(bool _bool) {
 	highlight = _bool;
 }
 
+/**
+ * Toggles the highligh boolean.
+ * If highlight = TRUE before the call to toggleHighlight()
+ * it will be FALSE after and vise versa.
+ */
 void GraphicalObject::toggleHighlight(){
 	highlight = !highlight;
 }
+
+/**
+ * Translates the object by the given offset by
+ * updating the pos variable.
+ * @param trans A Vec3 containing the offset values.
+ */
 void GraphicalObject::translate(Vec3 trans) {
 	Vec3 newPos = {pos.x+trans.x, pos.y+trans.y, pos.z+trans.z};
 	pos = newPos;
 }
 
+/**
+ * Sets the objectPos uniform so it can be accessed by the shader.
+ * @param shader The shader that uses the objectPos uniform.
+ */
 void GraphicalObject::applyTransformation(GLuint shader) {
     GLuint posLoc = glGetUniformLocation(shader, "objectPos");
     glUniform3f(posLoc, pos.x, pos.y, pos.z);
 }
 
+/**
+ * Sets the origin for the object. 
+ * The origin is the point in space which the object will rotate around.
+ * @param ori The new origin value.
+ */
 void GraphicalObject::setOrigin(Vec3 ori) {
         origin = ori;
 }
 
+/**
+ * Checks if the object is meshed.
+ * @return True if the object is meshed.
+ * False otherwise.
+ */
 bool GraphicalObject::hasMesh() {
 	return mesh;
 }
 
-void GraphicalObject::setMesh(bool set) {
-	mesh = set;
+/**
+ * Sets mesh to TRUE or FALSE.
+ * @param _mesh The new mesh boolean.
+ */
+void GraphicalObject::setMesh(bool _mesh) {
+	mesh = _mesh;
 }
 
-
+/**
+ * Scales the object by the provided factor.
+ * @param factor The factor for scaling.
+ */
 void GraphicalObject::resize(double factor) {
 	scale *= factor;
 }
 
+/**
+ * Sets the scale of the object.
+ * @param _scala The scale.
+ */
 void GraphicalObject::setScale(double _scale) {
 	scale = _scale;
 }
 
+/**
+ * Returns the scale of the object.
+ * @return The scale.
+ */
 double GraphicalObject::getScale() {
 	return scale;
 }
 
+/**
+ * Increments the objects scale.
+ * @param inc The increment.
+ */
 void GraphicalObject::incrementScale(double inc) {
 	scale += inc;
 }
 
 /**
-Tries to center the object around the given position. Relative to the cameras position.
-Doesent take rotation into account
+ * Tries to center the object around the given position. 
+ * Relative to the cameras position.
+ * Does not take rotation into account.
+ * @param camPos The cameras offset.
+ * @param optPos The point in space where the object will be centered around.
 */
-
 void GraphicalObject::center(Vec3 camPos, Vec3 optPos)
 {
 	Vec3 finalOffset;
@@ -206,26 +304,50 @@ void GraphicalObject::center(Vec3 camPos, Vec3 optPos)
 	pos = finalOffset;
 }
 
+/**
+ * Rotates the object around the x axis.
+ * @param angle The rotation angle in degrees.
+ */
 void GraphicalObject::rotateX(float angle) {
 	angleX+=angle;
 }
 
+/**
+ * Rotates the object around the y axis.
+ * @param angle The rotation angle in degrees.
+ */
 void GraphicalObject::rotateY(float angle) {
 	angleY+=angle;
 }
 
+/**
+ * Rotates the object around the z axis.
+ * @param angle The rotation angle in degrees.
+ */
 void GraphicalObject::rotateZ(float angle) {
 	angleZ+=angle;
 }
 
+/**
+ * Rotates the object around the x axis.
+ * @param angle The rotation angle in radians.
+ */
 void GraphicalObject::rotateXRad(float angle) {
 	//TODO
 }
 
+/**
+ * Rotates the object around the y axis.
+ * @param angle The rotation angle in radians.
+ */
 void GraphicalObject::rotateYRad(float angle) {
 	//TODO
 }
 
+/**
+ * Rotates the object around the z axis.
+ * @param angle The rotation angle in radians.
+ */
 void GraphicalObject::rotateZRad(float angle) {
 	//TODO
 }
