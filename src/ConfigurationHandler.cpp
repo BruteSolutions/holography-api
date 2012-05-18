a4f2a462c494f8a3e10711029c6747e80c4725fd
tompet0191 <skylines123@gmail.com>
2012-05-18 16:45:16 +0300

    Dokumentation

--- src/ConfigurationHandler.cpp
+++ src/ConfigurationHandler.cpp
@@ -9,14 +9,20 @@
 #include "ConfigurationHandler.h"
 using namespace std;
 
-/* Saves the configurations specified by the UniversalConfiguration object to a file specified by path. */
+/* Saves the configurations specified by the UniversalConfiguration object to a file specified by path. 
+ * @param uc The UniversalConfiguration to be saved
+ * @param path Path to where the configuration is supposed to be saved.
+ */
 void ConfigurationHandler::save( UniversalConfiguration uc, string path ){
     ofstream os( path );
     uc.writeToStream( os );
     os.close();
 }
 
-/* Loads the configurations in the file identified by path and returns them in a UniversalConfiguration object. */
+/* Loads the configurations in the file identified by path and returns them in a UniversalConfiguration object. 
+ * @param path Path to the configuration file
+ * @return UniversalConfiguration The UniversalConfiguration object created
+ */
 UniversalConfiguration ConfigurationHandler::load( string path )  throws ( std::string ) {
     //Check that the configuration we're trying to load actually exists
     std::ifstream ifile( path );
